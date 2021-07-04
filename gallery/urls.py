bc77ed89da3c32ffd2dda8576b4e5a0f1befcd0a
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-04 17:00:17 +0300

    Add upload images feature.

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views 
 
@@ -7,3 +9,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- galleryproject/urls.py
+++ galleryproject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('gallery/', include('gallery.urls')),
+    path('', include('gallery.urls')),
     path('admin/', admin.site.urls),
 ]
