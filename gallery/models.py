f29fded8f18d9697144f2bdf4728870150992429
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-04 06:35:26 +0300

    Register gallery app and make migrations.

--- gallery/models.py
+++ gallery/models.py
@@ -1,16 +1,5 @@
 from django.db import models
 
-class Image(models.Model):
-    '''Class to define attributes of an image in the gallery, and it's methods.
-    '''
-    image = models.ImageField(upload_to='images/')
-    name = models.CharField(max_length=150)
-    description = models.CharField(max_length=300)
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
-    post_date = models.DateTimeField(auto_now_add=True)
-
-
 class Location(models.Model):
     '''Class to define the location of a photo in the gallery.
     '''
@@ -26,3 +15,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    '''Class to define attributes of an image in the gallery, and it's methods.
+    '''
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=300)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+
