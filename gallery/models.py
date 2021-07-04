a492b3fdd633a999494dd78f679ed3dbe0c154db
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-04 06:25:36 +0300

    Add classes for model items Image, Category, and Location.

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    '''Class to define attributes of an image in the gallery, and it's methods.
+    '''
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=300)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+
+class Location(models.Model):
+    '''Class to define the location of a photo in the gallery.
+    '''
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    '''Class to define the category of an image on the gallery.
+    '''
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
+from .models import Image
 
-# Create your tests here.
+
