ad0480a69eb43f1005160273e0cd0e862e1b9058
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-04 12:21:01 +0300

    Add template.

--- gallery/views.py
+++ gallery/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world! You are at gallery index.")
+    return render(request, 'index.html')
