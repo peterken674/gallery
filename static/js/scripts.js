24fac7bb9f4fc400e6089e63225f9e61e140e1a6
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-04 16:37:07 +0300

    UI improvements.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,18 +1,20 @@
-$('.gallery ul li a').click(function () {
-    var itemID = $(this).attr('href');
-    $('.gallery ul').addClass('item_open');
-    $(itemID).addClass('item_open');
-    $('header').addClass('hide-header')
-    return false;
-});
-$('.close').click(function () {
-    $('.port, .gallery ul').removeClass('item_open');
-    $('header').removeClass('hide-header')
-    return false;
-});
+$(document).on('ready', function(){
+    $('.gallery ul li a').click(function () {
+        var itemID = $(this).attr('href');
+        $('.gallery ul').addClass('item_open');
+        $(itemID).addClass('item_open');
+        $('header').addClass('hide-header')
+        return false;
+    });
+    $('.close').click(function () {
+        $('.port, .gallery ul').removeClass('item_open');
+        $('header').removeClass('hide-header')
+        return false;
+    });
 
-$(".gallery ul li a").click(function () {
-    $('html, body').animate({
-        scrollTop: parseInt($("#top").offset().top)
-    }, 300);
+    $(".gallery ul li a").click(function () {
+        $('html, body').animate({
+            scrollTop: parseInt($("#top").offset().top)
+        }, 300);
+    });
 });
