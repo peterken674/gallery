1f1ff171c76f0f8237fc4815c5ff1adde93193dc
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-04 06:06:53 +0300

    Set up Postgresql database.

--- galleryproject/settings.py
+++ galleryproject/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'galleryproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'peterken',
+        'PASSWORD': 'pa55'
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
