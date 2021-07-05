49083759df50897fb6c9cd2e1ad1ea68fd658dc3
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-05 06:59:40 +0300

    Implement filtering by location functionality.

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,6 +8,10 @@ from . import views
 '''
 urlpatterns = [
     path('', views.index, name='index'),
+    path('kenya/', views.location_kenya, name='location_kenya'),
+    path('togo/', views.location_togo, name='location_togo'),
+    path('sierra/', views.location_sierra, name='location_sierra'),
+    path('egypt/', views.location_egypt, name='location_egypt'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- gallery/views.py
+++ gallery/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Category, Image
+from .models import Category, Image, Location
 
 def index(request):
     images = Image.objects.all()
     return render(request, 'index.html', {'images':images,})
 
-def search(request):
-
-    search_term = 'Travel'
-    found_category = Category.objects.filter(name=search_term).first()
-    found_images = Image.search_image(found_category)
+def location_kenya(request):
+    location_kenya = Location.objects.get(name='Kenya')
+    images = Image.objects.filter(location=location_kenya)
+    return render(request, 'index.html', {'images':images,})
+def location_togo(request):
+    location_togo = Location.objects.get(name='Togo')
+    images = Image.objects.filter(location=location_togo)
+    return render(request, 'index.html', {'images':images,})
+def location_sierra(request):
+    location_sierra = Location.objects.get(name='Sierra Leone')
+    images = Image.objects.filter(location=location_sierra)
+    return render(request, 'index.html', {'images':images,})
+def location_egypt(request):
+    location_egypt = Location.objects.get(name='Egypt')
+    images = Image.objects.filter(location=location_egypt)
+    return render(request, 'index.html', {'images':images,})
