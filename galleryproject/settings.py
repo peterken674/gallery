6d3ea95f679163ddd9cd308dc639f1cbefd14fbc
Peter Kennedy <peterken.ngugi@gmail.com>
2021-10-25 02:02:49 +0300

    Remove redundant settings.

--- galleryproject/settings.py
+++ galleryproject/settings.py
@@ -108,14 +108,14 @@ WSGI_APPLICATION = 'galleryproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gallery',
-        'USER': 'peterken',
-        'PASSWORD': 'pa55'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'gallery',
+#         'USER': 'peterken',
+#         'PASSWORD': 'pa55'
+#     }
+# }
 
 
 # Password validation
